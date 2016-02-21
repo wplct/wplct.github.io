f2e9e9657b69bf33d4e96b4c7d63fe74cb1436bf
wplct <wplct@qq.com>
2016-02-22 01:56:07 +0800

    .

--- live_bet.js
+++ live_bet.js
@@ -1,27 +1,26 @@
 /**
  * Created by wplct on 2016/2/20.
  */
-
-var $ = require('jquery');
-
-
-function getTokey(){
-    return $.cookie("LIVE_LOGIN_DATA")
-}
+function run(){
+    function getTokey(){
+        return $.cookie("LIVE_LOGIN_DATA")
+    }
 //ROOMURL
-function getRoomBet(){
-    $.get("http://live.bilibili.com/bet/getRoomBet",
-    {'roomid':ROOMURL},
-    function(data){
-        data=eval("("+data+")");
-        console.log(data);
-        alert(data);
-    })
-}
-function addBet (){
+    function getRoomBet(){
+        $.get("http://live.bilibili.com/bet/getRoomBet",
+            {'roomid':ROOMURL},
+            function(data){
+                data=eval("("+data+")");
+                console.log(data);
+                alert(data);
+            })
+    }
+    function addBet (){
 
-}
-console.log("wpl"+'test');
-getRoomBet();
+    }
+    console.log("wpl"+'test');
+    getRoomBet();
 
 //console.log(getTokey());
+}
+
